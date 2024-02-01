a3b6773bdd5d729ba76b9fb92cf86b4cbc1d022b
MarkBirdFly <3149255429@qq.com>
2024-02-01 23:16:11 +0800

    24.2.1 dev2

--- He_files/a.js
+++ He_files/a.js
@@ -1,14 +1,12 @@
 async function play(e, name) {
   createRipple(e);
   const buffer = await getBuffer("voices/" + name + ".mp3");
-  buffer && playAudio(buffer);
+  currentSource = playAudio(buffer);
 }
 function createRipple(event) {
   const button = event.currentTarget;
   const circle = document.createElement("div");
-  button.appendChild(circle);
-
-  // 计算水波纹的位置和大小
+  button.appendChild(circle);// 计算水波纹的位置和大小
   var x =
     event.pageX ||
     document.documentElement.scrollLeft +
@@ -32,19 +30,39 @@ function createRipple(event) {
     "px;left: " +
     x +
     "px";
-
   // 启动动画
   circle.classList.add("ripple");
   setTimeout(() => circle.remove(), 1000);
 }
+
+let activeSources = [];
+
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioContext();
 const playAudio = function (buffer) {
   const source = audioContext.createBufferSource();
   source.buffer = buffer;
   source.connect(audioContext.destination);
+  source.onended = function() {
+    // 当音频播放结束时，从数组中移除source对象
+    const index = activeSources.indexOf(source);
+    if (index > -1) {
+      activeSources.splice(index, 1);
+    }
+  };
   source.start();
+  activeSources.push(source); // 将此source对象添加到跟踪数组中
 };
+
+let currentSource = null;
+
+function stopAllAudio() {
+  activeSources.forEach(source => {
+    source.stop(); // 停止每个音频源
+  });
+  activeSources = []; // 清空数组，因为所有的音频都已停止
+}
+
 const getBuffer = function (url) {
   const request = new XMLHttpRequest();
   return new Promise((resolve, reject) => {
@@ -60,16 +78,16 @@ const getBuffer = function (url) {
   });
 };
 
-var ottocount = 0;
+var ottocount = 1;
 function otto(event) {
   let kj = event.currentTarget;
-  if (ottocount < 0) {
+  if (ottocount == 5) {
     play(event, "otto");
   }
   kj.classList.add("shaky");
     setTimeout(function () {
       kj.classList.remove("shaky");
-    }, 1000);
+    }, 500);
     ottocount++;
 }
 //Qmsg.info()
